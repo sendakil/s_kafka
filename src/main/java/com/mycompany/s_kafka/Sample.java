d67cbf0fee887c4a726e51bb4abccb102bb3ddb7
sendakil <sendakil@yahoo.com>
2020-10-02 16:59:38 +0800

    change in sample.java

--- src/main/java/com/mycompany/s_kafka/Sample.java
+++ src/main/java/com/mycompany/s_kafka/Sample.java
@@ -16,7 +16,8 @@ public class Sample {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Welcome!");
+        System.out.println(" Welcome!");
+        System.out.println(" To the world of Kafka");
     }
     
 }
